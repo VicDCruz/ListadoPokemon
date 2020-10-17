8c4ee3cbe04ba4c9f4f7d085fbaeec84b8ebecb1
Victor Cruz <vicdan215@gmail.com>
2020-10-17 14:46:15 -0500

    Change margins

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -13,11 +13,11 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import Header from 'containers/Header/Loadable';
 
 import GlobalStyle from '../../global-styles';
 
 const AppWrapper = styled.div`
-  max-width: calc(768px + 16px * 2);
   margin: 0 auto;
   display: flex;
   min-height: 100%;
@@ -27,21 +27,22 @@ const AppWrapper = styled.div`
 
 export default function App() {
   return (
-    <AppWrapper>
-      <Helmet
-        titleTemplate="%s - Listado de Pokémons"
-        defaultTitle="Listado de Pokémons"
-      >
-        <meta name="description" content="Una aplicación de Víctor Cruz" />
-      </Helmet>
-      {/* <Header /> */}
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        {/* <Route path="/features" component={FeaturePage} /> */}
-        <Route path="" component={NotFoundPage} />
-      </Switch>
-      {/* <Footer /> */}
-      <GlobalStyle />
-    </AppWrapper>
+    <div>
+      <Header />
+      <AppWrapper>
+        <Helmet
+          titleTemplate="%s - Listado de Pokémons"
+          defaultTitle="Listado de Pokémons"
+        >
+          <meta name="description" content="Una aplicación de Víctor Cruz" />
+        </Helmet>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          {/* <Route path="/features" component={FeaturePage} /> */}
+          <Route path="" component={NotFoundPage} />
+        </Switch>
+        <GlobalStyle />
+      </AppWrapper>
+    </div>
   );
 }
